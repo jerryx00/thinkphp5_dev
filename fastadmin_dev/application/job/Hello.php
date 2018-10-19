529a10c36e6def995836ffd89f39e544c9698f05
jerryxu <xiang37202@163.com>
2018-10-19 15:21:54 +0800

    init commit

---
+++ fastadmin_dev/application/job/Hello.php
@@ -0,0 +1,64 @@
+<?php
+namespace app\demo\job;
+/**
+* 文件路径： \app\demo\job\Hello.php
+* 这是一个消费者类，用于处理 helloJobQueue 队列中的任务
+*/
+use think\queue\Job;
+use think\Model;
+use think\Db;
+
+class Hello {
+
+    /**
+    * fire方法是消息队列默认调用的方法
+    * @param Job            $job      当前的任务对象
+    * @param array|mixed    $data     发布任务时自定义的数据
+    */
+    public function fire(Job $job,$data){
+
+        $isJobDone = $this->doHelloJob($data);
+
+        if ($isJobDone) {
+            //如果任务执行成功， 记得删除任务
+            $job->delete();
+            print("<info>Hello Job has been done and deleted"."</info>\n");
+        }else{
+            if ($job->attempts() > 3) {
+                //通过这个方法可以检查这个任务已经重试了几次了
+                print("<warn>Hello Job has been retried more than 3 times!"."</warn>\n");
+                $job->delete();
+                // 也可以重新发布这个任务
+                //print("<info>Hello Job will be availabe again after 2s."."</info>\n");
+                //$job->release(2); //$delay为延迟时间，表示该任务延迟2秒后再执行
+            }
+        }
+    }
+
+    /**
+    * 根据消息中的数据进行实际的业务处理
+    * @param array|mixed    $data     发布任务时自定义的数据
+    * @return boolean                 任务执行的结果
+    */
+    private function doHelloJob($data) {
+        // 根据消息中的数据进行实际的业务处理...
+        $list = Db::table('qw_goods_yt')->where(['status'=>1])->order('created_at')->select();
+        //        print("<info>Hello Job Started. job Data is: ".var_export($list,true)."</info> \n");
+        print(count($list) ." .task(s) \n");  
+        if ($list !== false) {
+            foreach ($list as $k => $v) {
+                $info = 'fluxid:'.$v['fluxid'].' ; goods:'.$v['fluxnum'].' ;price:'.$v['price'];
+                $filter_w['id'] = $v['id'];
+                $upd_data['status'] = 0 ;
+                $ret1 = Db::table('qw_goods_yt')->where($filter_w)->update($upd_data);
+                print('update ' .$ret1 ."\n");
+            }
+        }
+
+        //        print("<info>Hello Job Started. job Data is: ".var_export($data,true)."</info> \n");
+        //        print("<info>Hello Job is Fired at " . date('Y-m-d H:i:s') ."</info> \n");
+        //        print("<info>Hello Job is Done!"."</info> \n");
+
+        return false;
+    }
+}
---
+++ fastadmin_dev/application/job/MultiTask.php
@@ -0,0 +1,59 @@
+<?php
+/**
+* 文件路径： \application\index\job\MultiTask.php
+* 这是一个消费者类，用于处理 multiTaskJobQueue 队列中的任务
+*/
+namespace application\index\job;
+
+use think\queue\Job;
+use think\Model;
+use think\Db;
+
+class MultiTask {
+
+    public function taskA(Job $job,$data){
+        print("000000 .task(s) \n"); 
+        print("Begin to run TaskA \n");
+
+        $isJobDone = $this->_doTaskA($data);
+
+        if ($isJobDone) {
+            $job->delete();
+            print("Info: TaskA of Job MultiTask has been done and deleted"."\n");
+        }else{
+            if ($job->attempts() > 3) {
+                $job->delete();     
+            }
+        }
+    }
+
+    public function taskB(Job $job,$data){
+        print("Begin to run TaskB \n");
+
+        $isJobDone = $this->_doTaskB($data);
+
+        if ($isJobDone) {
+            $job->delete();
+            print("Info: TaskB of Job MultiTask has been done and deleted"."\n");
+        }else{
+            if ($job->attempts() > 2) {
+                print("Info: TaskB of Job MultiTask has been deleted deleted from attempts > 2"."\n");
+                $job->delete();     
+            }
+        }
+    }
+
+    private function _doTaskA($data) {
+        print("Info: doing TaskA of Job MultiTask "."\n");
+        //        $data_upd['iftype'] = ['exp', 'iftype+1'];
+        $data_upd['iftype'] = 'x';
+        $filter['id'] = ['eq','1'];
+        $rst = Db::table('qw_log')->where($filter)->update($data_upd);
+        return $ret        ;
+    }
+
+    private function _doTaskB($data) {
+        print("Info: doing TaskB of Job MultiTask "."\n");
+        return true;
+    }
+}
