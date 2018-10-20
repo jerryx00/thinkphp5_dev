a6124bfe2981cc190f9a4281604758dcd133f8eb
xiang37209 <xiang37209@163.com>
2018-10-20 22:47:43 +0800

    init commit

--- fastadmin_dev/application/api/controller/Llauth.php
+++ fastadmin_dev/application/api/controller/Llauth.php
@@ -7,108 +7,126 @@ use think\Exception;
 use think\Queue;
 
 
+
 /**
 * 流量认证接口
 */
 class Llauth extends LlfxApi
 {
 
-	protected $noNeedLogin = '*';
-	protected $noNeedRight = '*';
+    protected $noNeedLogin = '*';
+    protected $noNeedRight = '*';
 
-	public function _initialize()
-	{
-		parent::_initialize();
-	}
+    public function _initialize()
+    {
+        parent::_initialize();
+    }
 
-	public function index() {
-		$this->success('请求成功');
-	}
+    public function index() {
+        $this->success('请求成功');
+    }
 
-	public function order() {
-		$sign = input('sign');
-		$token = input('token');
+    public function order() {
+        $sign = input('sign');
+        $token = input('token');
 
-		$orderData = $this->getRaw();
-		$orderData =  file_get_contents("php://input") ;
-		$retC = json_decode($orderData);
-		$retC = object_array($retC);
-		$payload = $retC['payload']['data'];
-		$payload = $retC['header'];
+        $orderData = $this->getRaw();
+        $orderData =  file_get_contents("php://input") ; 
+        if ($orderData == '') {
+            $ret = $this->json_fail();
+            return $ret;
 
-		$orderId = date('YmdHis').rand(1000,9999);
+        }      
+        $retC = json_decode($orderData);
+        $retC = object_array($retC);
+        //        $ret = $this->checkDataValid($orderData);
 
-		// 1.当前任务将由哪个类来负责处理。
-		//   当轮到该任务时，系统将生成一个该类的实例，并调用其 fire 方法
-		$jobHandlerClassName  = 'app\api\job\ReceiveOrderQueue';
-		// 2.当前任务归属的队列名称，如果为新队列，会自动创建
-		$jobQueueName        = "receiveOrderQueue";
-		// 3.当前任务所需的业务数据 . 不能为 resource 类型，其他类型最终将转化为json形式的字符串
-		//   ( jobData 为对象时，需要在先在此处手动序列化，否则只存储其public属性的键值对)
-	//	$orderData             = [ 'ts' => time(), 'bizId' => uniqid()  ] ;
-		// 4.将该任务推送到消息队列，等待对应的消费者去执行
-		$isPushed = Queue::push( $jobHandlerClassName , $orderData , $jobQueueName );
-		// database 驱动时，返回值为 1|false  ;   redis 驱动时，返回值为 随机字符串|false
-		if( $isPushed !== false ){
-			$info['errcode'] = '0';
+        $payload = $retC['payload']['data'];
+        $payload = $retC['header'];
+
+        $orderId = date('YmdHis').rand(1000,9999);
+
+        // 1.当前任务将由哪个类来负责处理。
+        //   当轮到该任务时，系统将生成一个该类的实例，并调用其 fire 方法
+        $jobHandlerClassName  = 'app\api\job\ReceiveOrderQueue';
+        // 2.当前任务归属的队列名称，如果为新队列，会自动创建
+        //      php think queue:work --queue receiveOrderQueue
+        $jobQueueName        = "receiveOrderQueue";
+        // 3.当前任务所需的业务数据 . 不能为 resource 类型，其他类型最终将转化为json形式的字符串
+        //   ( jobData 为对象时，需要在先在此处手动序列化，否则只存储其public属性的键值对)
+        //	$orderData             = [ 'ts' => time(), 'bizId' => uniqid()  ] ;
+        // 4.将该任务推送到消息队列，等待对应的消费者去执行
+        $isPushed = Queue::push( $jobHandlerClassName , $orderData , $jobQueueName );
+        // database 驱动时，返回值为 1|false  ;   redis 驱动时，返回值为 随机字符串|false
+        if( $isPushed !== false ){
+            $info['errcode'] = '0';
             $info['errmsg'] = '提交成功';
-			$data['orderid'] = $orderId;
+            $data['orderid'] = $orderId;
 
-		}else{
-			$info['errcode'] = '9000001';
+        }else{
+            $info['errcode'] = '9000001';
             $info['errmsg'] = '提交失败';
             $data['orderid'] = '';
-		}
+        }
         $p['header'] = $info;
         $p['payload']['data'] = $data;
         return json($p);
 
 
-	}
+    }
+
+    protected function json_fail($errcode = '9000001', $errmsg='提交失败'){
+        $info['errcode'] = $errcode;
+        $info['errmsg'] = $errmsg;
+        $data['orderid'] = '';
+        $p['header'] = $info;
+        $p['payload']['data'] = $data;
+        return json($p);
+    }
 
 
 
-	/**
-	* 来自上游的回调
-	*
-	*/
-	public function notify() {
-		$sign = input('sign');
-		$token = input('token');
+    /**
+    * 来自上游的回调
+    *
+    */
+    public function notify() {
+        $sign = input('sign');
+        $token = input('token');
 
-		$orderData = $this->getRaw();
-		$retC = json_decode($orderData);
-		$retC = object_array($retC);
-		$payload = $retC['payload']['data'];
-		$payload = $retC['header'];
+        $orderData = $this->getRaw();
+        $retC = json_decode($orderData);
+        $retC = object_array($retC);
+        $payload = $retC['payload']['data'];
+        $payload = $retC['header'];
 
-		$orderId = date('YmdHis').rand(1000,9999);
+        $orderId = date('YmdHis').rand(1000,9999);
 
-		// 1.当前任务将由哪个类来负责处理。
-		//   当轮到该任务时，系统将生成一个该类的实例，并调用其 fire 方法
-		$jobHandlerClassName  = 'app\api\job\NotifyOrderQueue';
-		// 2.当前任务归属的队列名称，如果为新队列，会自动创建
-		$jobQueueName        = "notifyOrderQueue";
-		// 3.当前任务所需的业务数据 . 不能为 resource 类型，其他类型最终将转化为json形式的字符串
-		//   ( jobData 为对象时，需要在先在此处手动序列化，否则只存储其public属性的键值对)
-//		$orderData             = [ 'ts' => time(), 'bizId' => uniqid()  ] ;
-		// 4.将该任务推送到消息队列，等待对应的消费者去执行
-		$isPushed = Queue::push( $jobHandlerClassName , $orderData , $jobQueueName );
-		// database 驱动时，返回值为 1|false  ;   redis 驱动时，返回值为 随机字符串|false
-		if( $isPushed !== false ){
-			$info['errcode'] = '0';
+        // 1.当前任务将由哪个类来负责处理。
+        //   当轮到该任务时，系统将生成一个该类的实例，并调用其 fire 方法
+        $jobHandlerClassName  = 'app\api\job\NotifyOrderQueue';
+        // 2.当前任务归属的队列名称，如果为新队列，会自动创建
+        $jobQueueName        = "notifyOrderQueue";
+        // 3.当前任务所需的业务数据 . 不能为 resource 类型，其他类型最终将转化为json形式的字符串
+        //   ( jobData 为对象时，需要在先在此处手动序列化，否则只存储其public属性的键值对)
+        //		$orderData             = [ 'ts' => time(), 'bizId' => uniqid()  ] ;
+        // 4.将该任务推送到消息队列，等待对应的消费者去执行
+        $isPushed = Queue::push( $jobHandlerClassName , $orderData , $jobQueueName );
+        // database 驱动时，返回值为 1|false  ;   redis 驱动时，返回值为 随机字符串|false
+        if( $isPushed !== false ){
+            $info['errcode'] = '0';
             $info['errmsg'] = '提交成功';
-			$data['orderid'] = $orderId;
+            $data['orderid'] = $orderId;
 
-		}else{
-			$info['errcode'] = '9000001';
+        }else{
+            $info['errcode'] = '9000001';
             $info['errmsg'] = '提交失败';
             $data['orderid'] = '';
-		}
+        }
         $p['header'] = $info;
         $p['payload']['data'] = $data;
         return json($p);
 
-	}
+    }
 
 }
---
+++ fastadmin_dev/application/api/job/ReceiveOrderQueue1.php
@@ -0,0 +1,115 @@
+<?php
+namespace app\api\job;
+
+/**
+* 文件路径： \app\demo\job\Hello.php
+* 这是一个消费者类，用于处理 helloJobQueue 队列中的任务
+*/
+use think\queue\Job;
+use think\Model;
+use think\Db;
+
+class ReceiveOrderQueue {
+    
+
+	/**
+	* fire方法是消息队列默认调用的方法
+	* @param Job            $job      当前的任务对象
+	* @param array|mixed    $data     发布任务时自定义的数据
+	*/
+	public function fire(Job $job,$data){
+
+		// 如有必要,可以根据业务需求和数据库中的最新数据,判断该任务是否仍有必要执行.
+		$isJobStillNeedToBeDone = $this->checkDatabaseToSeeIfJobNeedToBeDone($data);
+		if(!isJobStillNeedToBeDone){
+			$job->delete();
+			print("<info Job has been checked fail and deleted"."</info>\n");
+			return;
+		}
+
+		$isJobDone = $this->dealOrderJob($data);
+
+		if ($isJobDone['errcode'] == '0') {
+
+			//如果任务执行成功， 记得删除任务
+			$job->delete();
+
+		}else{
+
+			//			if ($job->attempts() > 3) {
+			//通过这个方法可以检查这个任务已经重试了几次了
+
+			//				$job->delete();
+			// 也可以重新发布这个任务
+			//print("<info>Hello Job will be availabe again after 2s."."</info>\n");
+			//$job->release(2); //$delay为延迟时间，表示该任务延迟2秒后再执行
+			//			}
+			$job->delete();
+		}
+
+	}
+
+	/**
+	* 根据消息中的数据进行实际的业务处理
+	* @param array|mixed    $data     发布任务时自定义的数据
+	* @return boolean                 任务执行的结果
+	*/
+	private function dealOrderJob($data) {
+
+		print("<info>Hello Job Started. job Data is: ".var_export($data,true)."</info> \n");
+		print("<info>Hello Job is Fired at " . date('Y-m-d H:i:s') ."</info> \n");
+		print("<info>Hello Job is Done!"."</info> \n");
+
+		$info['errcode'] = '0';
+		$info['errmsg'] = '';
+		// 根据消息中的数据进行实际的业务处理...
+		//检验黑白名单
+		//        $ret1 = $this->checkIPValid();
+		//检查订单商品是否存在
+		//        $ret1 = $this->checkGoodsValid();
+		//校验订单号是否重复提交
+		//        $ret2 = $this->checkOrdernoValid();
+		//检查费用是否充足
+		//        $ret3 = $this->checkFeeValid();
+         //订单提交给供货商
+		//$ret3 = $this->postToSupplier();
+		//如果提交失败，再直接把订单丢到NotifyOrderQueue队列中
+
+		//$list = Db::table('qw_goods_yt')->where(['status'=>1])->order('created_at')->select();
+//		if ($list !== false) {
+			return $info;
+//		}  else {
+//			$info['errcode'] = '100001';
+//			$info['errmsg'] = '商品不存在';
+//			return $info;
+//		}
+	}
+
+	/**
+	* 数组校验、检查、扣费等逻辑在此实现
+	*
+	* 有些消息在到达消费者时,可能已经不再需要执行了
+	* @param array|mixed    $data     发布任务时自定义的数据
+	* @return boolean                 任务执行的结果
+	*/
+	private function checkDatabaseToSeeIfJobNeedToBeDone($data){
+		return true;
+	}
+
+	/**
+	* Log a failed job into storage.
+	* @param  \Think\Queue\Job $job
+	* @return array
+	*/
+	protected function logFailedJob(Job $job)
+	{
+		//将原来的 queue.failed' 修改为 'queue_failed' 才可以触发任务失败回调
+		if (Hook::listen('queue_failed', $job, null, true)) {
+			$job->delete();
+			$job->failed();
+		}
+
+		return ['job' => $job, 'failed' => true];
+	}
+
+}
